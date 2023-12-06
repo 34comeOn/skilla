0cbd8d5c073642ff7df8f00d360bf76019f5579f
Mikhail <barabanovm87@gmail.com>
2023-12-06 16:36:33 +0300

    creates main menu buttons

--- src/modules/mainMenu/index.tsx
+++ src/modules/mainMenu/index.tsx
@@ -1,5 +1,8 @@
 import { Logo } from './components/logo';
+import { MenuButton } from './components/menuButton';
 import { Navigation } from './navigation';
+import AddOrder from 'src/accets/icons/add-order.svg';
+import Pay from 'src/accets/icons/pay.svg';
 import './style.scss';
 
 export const MainMenu = () => {
@@ -7,6 +10,8 @@ export const MainMenu = () => {
         <div className='menu__container'>
             <Logo />
             <Navigation />
+            <MenuButton title='Добавить заказ' alt='add order' icon={AddOrder} buttonPadding='34px'/>
+            <MenuButton title='Оплата' alt='pay' icon={Pay} buttonPadding='30px'/>
         </div>
     )
 }
